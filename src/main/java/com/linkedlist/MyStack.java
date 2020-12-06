6b40480d1416f2440f2c07deb6ff0b10e460a9df
ManaliParab12 <manaliparab10@gmail.com>
2020-12-06 20:42:51 +0530

    Popping all the nodes till stack is empty

--- src/main/java/com/linkedlist/MyStack.java
+++ src/main/java/com/linkedlist/MyStack.java
@@ -4,21 +4,6 @@ public class MyStack {
 
     private final MyLinkedList myLinkedList;
 
-    public static void main(String[] args){
-        MyStack myStack = new MyStack();
-        MyNode<Integer> myFirstNode = new MyNode<>(70);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(56);
-        myStack.push(myFirstNode);
-        myStack.push(mySecondNode);
-        myStack.push(myThirdNode);
-        myStack.printStack();
-      //  myStack.peak();
-      //  myStack.pop();
-      //  myStack.length();
-      // myStack.isEmpty();
-      myStack.makeStackEmpty();
-    }
     public MyStack() {
         this.myLinkedList = new MyLinkedList();
     }
